23573d1ad010555516918a3dbbaec8aee3893190
Emily Evans <emilyevans@wifi-roaming-128-4-111-15.host.udel.edu>
2019-09-24 11:20:16 -0400

    I dont know man

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+	include("index.html");
+	include("index.css");
+	include("index.js");
     //this is the basic way of getting a database handler from PDO, PHP's built in quasi-ORM
     $dbhandle = new PDO("sqlite:scrabble.sqlite") or die("Failed to open DB");
     if (!$dbhandle) die ($error);
