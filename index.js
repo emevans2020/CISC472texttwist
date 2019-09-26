9712e085102af2819225083304089912f1737c82
Emily Evans <emilyevans@wifi-roaming-128-4-108-62.host.udel.edu>
2019-09-26 16:42:40 -0400

    working???

--- index.js
+++ index.js
@@ -106,7 +106,7 @@ var TextTwist = function() {
         document.getElementById("start").addEventListener('click', function(){
             this.disabled = true;
             this.textContent = 'Loading...';
-            genericGetRequest("index.php", self.processResult);
+            genericGetRequest("api/index.php", self.processResult);
         });
     };
     init();
